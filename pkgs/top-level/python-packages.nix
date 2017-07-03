639e539a795291c0bca5aad0700d5e3d5fee88d2
Langston Barrett <langston.barrett@gmail.com>
2017-07-03 23:31:49 +0000

    ukpostcodeparser: init at 1.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -24496,6 +24496,8 @@ in {
 
   u-msgpack-python = callPackage ../development/python-modules/u-msgpack-python { };
 
+  ukpostcodeparser = callPackage ../development/python-modules/ukpostcodeparser { };
+
   umalqurra = buildPythonPackage rec {
     name = "umalqurra-${version}";
     version = "0.2";
